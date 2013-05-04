0df371ab5d39fa154f4db8b3bb8f9dea38e96565
Amitay Isaacs <amitay@gmail.com>
2013-05-05 01:28:20 +1000

    EA: Update to use modified problem definition and eval_mask

--- @EA/eval_pop_surr.m
+++ @EA/eval_pop_surr.m
@@ -8,25 +8,52 @@ function [ea, pop] = eval_pop_surr(ea, pop, surr, varargin)
 		id = 1:pop.size;
 	end
 	
-	% Evaluate population
-	fn_evals = 0;
-
+	if strcmp(ea.analysis.type, 'composite')
+		fn_evals = 0;
+		f_mask = [];
+		g_mask = [];
+	else
+		fn_evals = zeros(1, ea.prob.nf+ea.prob.ng);
+		f_mask = zeros(1, ea.prob.nf);
+		g_mask = zeros(1, ea.prob.ng);
+		
+		if isempty(ea.prob.eval_mask)
+			surr_f_mask = zeros(1, ea.prob.nf);
+			surr_g_mask = zeros(1, ea.prob.ng);
+		else
+			surr_f_mask = ea.prob.eval_mask(1:ea.prob.nf);
+			surr_g_mask = ea.prob.eval_mask(ea.prob.nf+1:end);
+		end
+		surr_fn_evals = 1 - [surr_f_mask surr_g_mask];
+	end
+	
 	state = [];
 	state.gen_id = ea.gen_id;
 	state.userdata = ea.prob.userdata;
 
+	% Evaluate population
 	for i = id 
 		state.pop_id = i;
 		if get_evalflag(pop,i) == 0
 			x = convert_x(ea.object, get_x(pop,i));
 			[y, valid] = predict(surr, x);
 			if valid == 1
-				f = y(1:pop.nf);
-				g = y(pop.nf+1:end);
+				if strcmp(ea.analysis.type, 'composite')
+					f = y(1:pop.nf);
+					g = y(pop.nf+1:end);
+				else
+					f1 = y(1:pop.nf);
+					g1 = y(pop.nf+1:end);
+					x = get_x(pop, i);
+					[ea, ~, f2, g2] = eval_cache(ea, x, 1, state, surr_f_mask, surr_g_mask);
+					[f, g] = merge_fg(f1, g1, f2, g2, surr_f_mask, surr_g_mask);
+					fn_evals = fn_evals + surr_fn_evals;
+				end
 				pop = assign_fitness(pop, i, f, g);
 				pop = set_evalflag(pop, i, 2);
 			else
-				[ea, x, f, g] = eval_cache(ea, get_x(pop,i), 1, state);
+				x = get_x(pop, i);
+				[ea, x, f, g] = eval_cache(ea, x, 1, state, f_mask, g_mask);
 				pop = set_x(pop, i, x);
 				pop = assign_fitness(pop, i, f, g);
 				pop = set_evalflag(pop, i, 1);
@@ -36,4 +63,18 @@ function [ea, pop] = eval_pop_surr(ea, pop, surr, varargin)
 	end
 	
 	ea = add_fnevals(ea, fn_evals);
+end
+
+
+function [f, g] = merge_fg(f, g, f2, g2, f_mask, g_mask)
+	for i = 1:length(f_mask)
+		if f_mask(i) == 0
+			f(i) = f2(i);
+		end
+	end
+	for i = 1:length(g_mask)
+		if g_mask(i) == 0
+			g(i) = g2(i);
+		end
+	end
 end
